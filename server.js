2b41c30b1db1f35c91267949ff0cf17313ee03c5
HaileyThomas <haileyraebeauty@gmail.com>
2022-07-25 15:24:40 -0700

    connect to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "library",
+  },
+  console.log("Connected to the library database.")
+);
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
